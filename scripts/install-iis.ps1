ff460bd2d005fc76e9759ceccffac748070a6506
tth <tzehon.tan@gmail.com>
2021-04-16 19:34:35 +0800

    Update landing page

--- scripts/install-iis.ps1
+++ scripts/install-iis.ps1
@@ -20,4 +20,7 @@ Add-WindowsFeature $features -Verbose
 Write-Host "Opening port 80..."
 netsh advfirewall firewall add rule name="open_80_api" dir=in localport=80 protocol=TCP action=allow
 
+Set-Content -Path C:\inetpub\wwwroot\index.html -Value '<!doctype html><html><body><h1>Hello World!</h1></body></html>'
+Get-Content -Path C:\inetpub\wwwroot\index.html
+
 choco install -y webdeploy
